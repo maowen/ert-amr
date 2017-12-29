10cbd77f00c8889680a1bb3a61ba62bcbc3d7c70
Mark Owen <mowen01@gmail.com>
2017-12-29 00:38:36 -0700

    Removed printf redefinition that is unneeded when using the ESP8266 Arduino framework

--- ezradio/platform/esp8266/amr_hal.h
+++ ezradio/platform/esp8266/amr_hal.h
@@ -7,7 +7,6 @@ void amrHalInit();
 uint8_t amrHalRunning();
 void amrHalEnable(uint8_t enable);
 
-#define printf os_printf_plus // Override the default printf definition
 #define usleep ets_delay_us // Override the default usleep definition
 
 #endif
